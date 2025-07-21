1755517b0f08a8f87591fdc29bfca22edc111902
Andyll <blessleydylanworkmail@proton.me>
2025-07-21 12:15:42 +0800

    Delete Tests File

--- labs/lab03/tests/test_lab03.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-def test_lab03_prints_hello():
-    output = subprocess.check_output([sys.executable, "labs/lab03/exercise.py"])
-    assert b"Hello, Lab 03" in output
