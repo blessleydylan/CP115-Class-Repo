ff7cee7fb184b4b548cf99554bf3cc839c702ff0
DYLAN <blessleydylanworkmail@proton.me>
2025-08-11 12:48:52 +0800

    Complete Lab Test 02

---
+++ labs/lab-test-2.py
@@ -0,0 +1,18 @@
+# Programmer Name    : Blessley Dylan
+# Problem Description: To produce output using appropriate escape characters and arithmetic expression
+
+# Set up Variables such as Name and Matric. No and full_mark
+
+name = "Ahmad Bin Abu"
+matric_no = "MS2025123499"
+full_mark = 2 * 10
+
+# Print name and matric.no
+print("Name: " ,name , "\t\t\t" "Matric. No: " ,matric_no, "\n\n")
+
+# Print the image required
+print("*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n")
+
+# Print text with data generated using arithmetic expression
+print('''This is my\n\tsecond\n\t\tassignment''')
+print("I want 2x10 marks, which is", full_mark,"full marks")
