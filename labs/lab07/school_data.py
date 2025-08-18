7adef5fd95e045056dcb20380997caba62ce88ce
DYLAN <blessleydylanworkmail@proton.me>
2025-08-18 12:46:27 +0800

    Complete School Data File

---
+++ labs/lab07/school_data.py
@@ -0,0 +1,17 @@
+# school_data.py
+import random
+import math
+
+# Student information
+student_name = "Aieman"
+student_id = "C02"
+age = 18
+
+# Course information
+courseCode = "JSK"
+coursename = "Computer Science"
+
+# String operations
+NAMeupper = student_name.upper()
+namelwr = student_name.lower()
+name_length = len(student_name)
