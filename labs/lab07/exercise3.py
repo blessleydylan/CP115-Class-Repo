695b683a990955f9fe6e7c66ca7a4ad31ec96733
DYLAN <blessleydylanworkmail@proton.me>
2025-08-18 12:38:39 +0800

    Update Exercise 3

--- labs/lab07/exercise3.py
+++ labs/lab07/exercise3.py
@@ -5,5 +5,5 @@ import shopping_data
 print("=== Using My Own Module ===")
 print(f"Product Name: {shopping_data.product_name}")
 print(f"Price: {shopping_data.price}")
-print(f"Quantity: {shopping_data.quantity}%")
-print(f"Total Cost: {shopping_data.total_price}%")
+print(f"Quantity: {shopping_data.quantity}")
+print(f"Total Cost: {shopping_data.total_cost}%")
