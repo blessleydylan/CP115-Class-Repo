d1da29251a3f399449a4c0b2c96dd546a9af7e36
DYLAN <blessleydylanworkmail@proton.me>
2025-08-18 12:38:04 +0800

    Complete exercise 3

---
+++ labs/lab07/exercise3.py
@@ -0,0 +1,9 @@
+# shopping_data.py
+import shopping_data
+
+# Use variables from your own module
+print("=== Using My Own Module ===")
+print(f"Product Name: {shopping_data.product_name}")
+print(f"Price: {shopping_data.price}")
+print(f"Quantity: {shopping_data.quantity}%")
+print(f"Total Cost: {shopping_data.total_price}%")
---
+++ labs/lab07/shopping_data.py
@@ -0,0 +1,8 @@
+# shopping_data.py
+# This is your own module with useful data
+
+# Student information
+product_name = "Egg"
+price = 8
+quantity = 10
+total_cost = price * quantity
