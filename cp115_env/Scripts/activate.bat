6c3e874aa1d655f3a8487f8e8f316141665ccb05
Andyll <blessleydylanworkmail@proton.me>
2025-09-08 13:27:50 +0800

    Commit

--- cp115_env/Scripts/activate.bat
+++ cp115_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Documents\Tuaran Punya\Computer Programming\CP115-Class-Repo\cp115_env
+set VIRTUAL_ENV=C:\Users\User\OneDrive\Documents\Computer Programming\CP115-Class-Repo\cp115_env
 
 if not defined PROMPT set PROMPT=$P$G
 
