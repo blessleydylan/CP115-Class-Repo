56404833a6b7967af01c56397765e419b3cda856
Andyll <blessleydylanworkmail@proton.me>
2025-09-22 11:58:15 +0800

    Try Lab 01 Exercise 1

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -3,6 +3,20 @@ overtime_hours = int(input())
 is_weekend = input()
 
 # Your code here
+if position == "Manager":
+    hourly_rate = 35
+elif position == "Supervisor":
+    hourly_rate = 25    
+else:
+    hourly_rate = 18
+
+if is_weekend == "Yes":
+    hourly_rate += 5
+overtime_pay = overtime_hours * (hourly_rate * 1.5)
+total_pay = overtime_pay
+
+
+
 
 print(hourly_rate)
 print(overtime_pay)
