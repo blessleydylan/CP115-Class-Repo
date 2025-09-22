ca98d77646da8e65da708304d923aa94a899feb7
Andyll <blessleydylanworkmail@proton.me>
2025-09-22 12:57:44 +0800

    Complete Exercise 3 Lab 10

--- labs/lab10/exercise3/exercise3.py
+++ labs/lab10/exercise3/exercise3.py
@@ -4,14 +4,15 @@ loan_amount = int(input())
 
 # Your code here
 
-max_loan_amount = monthly_income * 5
-approval_status = "Approved" if monthly_income >= 4000 and credit_score >= 600 and loan_amount <= max_loan_amount else "Rejected"
-
-if approval_status == "Approved":
-    interest_rate = 3.5 if credit_score >= 700 else 5.5
+if credit_score >= 700:
+    interest_rate = 3.5
+elif credit_score >= 600:
+    interest_rate = 5.5
 else:
     interest_rate = 0.0
-
+    
+max_loan_amount = monthly_income * 5
+approval_status = "Approved" if monthly_income >= 4000 and credit_score >= 600 and loan_amount <= max_loan_amount else "Rejected"
 
 print(interest_rate)
 print(max_loan_amount)
